2a99267db544d01ebc369590535e8e647f7e3997
deorsi <deorsi@protonmail.com>
2019-11-01 10:06:07 -0300

    Updates

---
+++ precache-manifest.ece05cfd574f075fd0adb8e7eddac927.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a24dc1b58be6aabefe8bc3c2d6fdd14",
+    "url": "/infocards/index.html"
+  },
+  {
+    "revision": "15df6e8dd3640d6f8ec6",
+    "url": "/infocards/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "ea8141b72a87bdf112a3",
+    "url": "/infocards/static/css/main.cc0e8367.chunk.css"
+  },
+  {
+    "revision": "15df6e8dd3640d6f8ec6",
+    "url": "/infocards/static/js/2.85d9ebd3.chunk.js"
+  },
+  {
+    "revision": "ea8141b72a87bdf112a3",
+    "url": "/infocards/static/js/main.9b8fcf4f.chunk.js"
+  },
+  {
+    "revision": "5963ac770053346e7842",
+    "url": "/infocards/static/js/runtime-main.5fa87faa.js"
+  },
+  {
+    "revision": "a64ed299ab6012fdd5d8b0d5c3e9236e",
+    "url": "/infocards/static/media/Viafont.a64ed299.woff"
+  }
+]);
