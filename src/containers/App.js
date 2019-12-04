a047eb6cceec408751f4b0ff94bae319b3980ced
Deorsi <47460945+deorsi@users.noreply.github.com>
2019-12-04 16:37:22 -0200

    Add files via upload

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -40,7 +41,7 @@ class App extends Component {
             <h1>Loading</h1> :
             (
             <div className='tc'>   
-                <h1 className='f1'>Infocards</h1>
+                <Header />
                 <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
