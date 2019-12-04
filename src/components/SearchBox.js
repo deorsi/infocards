f65419480663423c1baac21886e702aaaa3b54e5
Deorsi <47460945+deorsi@users.noreply.github.com>
2019-12-04 18:42:19 -0200

    optimizing to PWA

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,10 +3,11 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
     return (
         <div className='pa2'>
-            <input 
+            <input
+                aria-label="Search Infocards"
                 className='pa3 ba b-lightest-blue: bg-lightest-blue'
-                type='search' 
-                placeholder='search your infocard'
+                type='search'
+                placeholder='Search your Infocard'
                 onChange={searchChange}
             />
         </div>
